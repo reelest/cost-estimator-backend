1f3964b48342d7f422543a7e46b43cb1e54f6815
ROWEND36 <rowendduke36@gmail.com>
2023-04-23 04:23:34 +0100

    added flask

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
 from flask import Flask
-
+from prefix_middleware import PrefixMiddleware
 app = Flask(__name__)
+app.debug = True
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix="/api")
 
 @app.route("/")
 def hello_world():

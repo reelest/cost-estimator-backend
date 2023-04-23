b6f76857dfa08eef6a9d05cfa33838a00e9482d3
ROWEND36 <rowendduke36@gmail.com>
2023-04-23 04:29:01 +0100

    fix: changed vercel.json to use rewrites

--- api/index.py
+++ api/index.py
@@ -1,8 +1,6 @@
 from flask import Flask
-from prefix_middleware import PrefixMiddleware
+
 app = Flask(__name__)
-app.debug = True
-app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix="/api")
 
 @app.route("/")
 def hello_world():

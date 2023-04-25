46cca8aa9a46826f10a3e509f58ccc76f27f57d5
ROWEND36 <rowendduke36@gmail.com>
2023-04-25 12:02:23 +0100

    feat: deploy

---
+++ api/cost_estimate/train_model.py
@@ -0,0 +1,55 @@
+import numpy as np
+from sklearn.svm import SVR
+import matplotlib.pyplot as plt
+import pandas as pd
+
+data = pd.read_csv("./data.csv", header=None)
+X = data[[i for i in range(20)]]
+Y = data[20]
+
+svr_rbf = SVR(kernel="rbf", C=100, gamma=0.1, epsilon=0.1)
+svr_lin = SVR(kernel="linear", C=100, gamma="auto")
+svr_poly = SVR(kernel="poly", C=100, gamma="auto", degree=3, epsilon=0.1, coef0=1)
+
+svrs = [svr_rbf, svr_lin, svr_poly]
+
+kernel_label = ["RBF", "Linear", "Polynomial"]
+model_color = ["m", "c", "g"]
+
+fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15, 10), sharey=True)
+for ix, svr in enumerate(svrs):
+    axes[ix].plot(
+        X,
+        svr.fit(X, Y).predict(X),
+        color=model_color[ix],
+        lw=2,
+        label="{} model".format(kernel_label[ix]),
+    )
+    axes[ix].scatter(
+        X[svr.support_],
+        Y[svr.support_],
+        facecolor="none",
+        edgecolor=model_color[ix],
+        s=50,
+        label="{} support vectors".format(kernel_label[ix]),
+    )
+    axes[ix].scatter(
+        X[np.setdiff1d(np.arange(len(X)), svr.support_)],
+        Y[np.setdiff1d(np.arange(len(X)), svr.support_)],
+        facecolor="none",
+        edgecolor="k",
+        s=50,
+        label="other training data",
+    )
+    axes[ix].legend(
+        loc="upper center",
+        bbox_to_anchor=(0.5, 1.1),
+        ncol=1,
+        fancybox=True,
+        shadow=True,
+    )
+
+fig.text(0.5, 0.04, "data", ha="center", va="center")
+fig.text(0.06, 0.5, "target", ha="center", va="center", rotation="vertical")
+fig.suptitle("Support Vector Regression", fontsize=14)
+plt.show()

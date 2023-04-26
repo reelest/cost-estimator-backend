db458e96f14dd176269470c444d449b9528a894c
ROWEND36 <rowendduke36@gmail.com>
2023-04-26 13:09:18 +0100

    feat: deploy

--- api/_utils/predict.py
+++ api/_utils/predict.py
@@ -1,13 +1,16 @@
 import os
 import pickle
 import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+
 _model = None
 def get_model():
+    global _model
     if not _model:
         with open(f"{os.path.dirname(__file__)}/model.pickle", 'rb') as fp:
             _model = pickle.load(fp)
     return _model
 
 def predict(arr):
-    model = get_model()
-    return list(model.predict(np.array(arr)))
+    model:RandomForestRegressor = get_model()
+    return model.predict(np.array(arr).reshape(1, 20)).reshape(-1).tolist()

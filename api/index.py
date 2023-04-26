6cf80ceeab5c41f98c3f0975e69dfc86e4ee644e
ROWEND36 <rowendduke36@gmail.com>
2023-04-26 20:19:38 +0100

    feat: cors

--- api/index.py
+++ api/index.py
@@ -1,15 +1,19 @@
 from flask import Flask, request, abort,jsonify
+from flask_cors import CORS
+
 from ._utils.predict import predict
 
 app = Flask(__name__)
+CORS(app)
 
-
-@app.route("/")
+@app.post("/")
 def get_cost_map():
     content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
-        response = response = jsonify(predict(request.json))
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
+    if content_type and'application/json' in content_type:
+        return predict(request.json)
     else:
         abort(401, 'Content-Type not supported!')
+
+app.get("/")
+def status():
+    return "Server is running"
